4535a425f8a18acca52382c2ab25257c6cdec87a
Stella Maris González Robert <stegonyrob@yahoo.es>
2025-05-05 14:49:01 +0200

    delete contact form by email

--- src/assets/Views/AboutMeView.tsx
+++ src/assets/Views/AboutMeView.tsx
@@ -1,12 +1,12 @@
 
 import CardText from "../Components/Card/CardText";
-import ContactForm from "../Components/ContactMe/ContactForm";
+
 import styles from './scss/Views.module.scss';
 const AboutMeView = () => {
   return (
     <div className={styles.container}>
       <CardText ids={['15']} />
-      <ContactForm />
+
     </div>
   );
 };
